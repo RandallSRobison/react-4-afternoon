5e5c36a659c7fa870f6981b432e8b9aaa434165d
Spence Robison <spence@MacBook-Pro.local>
2019-07-02 16:43:56 -0600

    Finished all and both Black Diamonds

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
+    this.state = {
+      students: []
+    }
+    this.goBack = this.goBack.bind(this)
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(res => {
+        this.setState({
+          students: res.data
+        })
+      })
+  }
+
+  goBack() {
+    this.props.history.goBack()
   }
 
   render() {
+    let students = this.state.students.map((student, i) => (
+    <Link to={`/student/${student.id}`} key={i}>
+      <h3>
+        {student.first_name} {student.last_name}
+      </h3>
+    </Link>
+    ))
     return (
       <div className="box">
-        <h1></h1>
+        <button onClick={this.goBack}>Go Back</button>
+        {/* <Link to=''>Go Back</Link> */}
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+// import {Link} from 'react-router-dom'
+import axios from 'axios';
+
 
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo: {}
+    }
+    this.goBack = this.goBack.bind(this)
+  }
+
+  componentDidMount() {
+    return axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(res => {
+        this.setState({
+          studentInfo: res.data
+        })
+      })
+  }
+
+  goBack() {
+    this.props.history.goBack()
   }
 
   render() {
     return (
       <div className="box">
+          <button onClick={this.goBack}>Go Back</button>
+          {/* <Link to={`/classlist/${this.state.studentInfo.class}`}>Go back</Link> */}
         <h1>Student</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
       </div>
     )
   }
